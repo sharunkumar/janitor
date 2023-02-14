d51268e65d08bd8fe885921493321e8bef250b9e
Sharun <sharunksplus@gmail.com>
2023-02-13 23:08:59 -0500

    added the destination to the configuration

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,20 @@ use std::path::Path;
 use std::thread;
 use std::time::Duration;
 
-const DESTINATION_FOLDER: &str = "E:\\janitor\\";
-
 fn main() {
     let user_dirs = UserDirs::new().unwrap();
     let downloads_path = user_dirs.download_dir().unwrap();
-    let destination_path = Path::new(DESTINATION_FOLDER);
-
-    let config = ini!(downloads_path.join("janitor.ini").to_str().unwrap());
-
-    if !destination_path.exists() {
-        fs::create_dir_all(destination_path).unwrap();
-    }
 
     loop {
-        // Check if the file is in the downloads folder
+        let config = ini!(downloads_path.join("janitor.ini").to_str().unwrap());
+
+        // get the first value in config["destination"]
+        let desination = config["destination"]["default"].clone().unwrap();
+        let destination_path = Path::new(&desination);
+
+        if !destination_path.exists() {
+            fs::create_dir_all(destination_path).unwrap();
+        }
 
         for file in config["sources"]
             .values()
@@ -28,7 +27,6 @@ fn main() {
             let file_str = file.to_owned();
 
             if downloads_path.join(&file_str).exists() {
-                // Check if the file is still being written to
                 fs::copy(
                     downloads_path.join(&file_str),
                     destination_path.join(&file_str),
@@ -37,7 +35,7 @@ fn main() {
 
                 fs::remove_file(downloads_path.join(&file_str)).unwrap();
 
-                println!("Moved {} to {}.", &file_str, DESTINATION_FOLDER);
+                println!("Moved {} to {}.", &file_str, &desination);
             }
         }
 
