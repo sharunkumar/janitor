cdb50d6f00525d59c29f93751c113e68a7af3e08
Sharun <sharunksplus@gmail.com>
2023-02-13 22:49:27 -0500

    read file list from ini in downloads directory

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use directories::UserDirs;
+use ini::ini;
 use std::fs;
 use std::path::Path;
 use std::thread;
 use std::time::Duration;
 
-const FILE_NAME: &str = "example.txt";
 const DESTINATION_FOLDER: &str = "E:\\janitor\\";
 
 fn main() {
     let user_dirs = UserDirs::new().unwrap();
     let downloads_path = user_dirs.download_dir().unwrap();
     let destination_path = Path::new(DESTINATION_FOLDER);
+
+    let config = ini!(downloads_path.join("janitor.ini").to_str().unwrap());
 
     if !destination_path.exists() {
         fs::create_dir_all(destination_path).unwrap();
@@ -19,17 +21,24 @@ fn main() {
     loop {
         // Check if the file is in the downloads folder
 
-        if downloads_path.join(FILE_NAME).exists() {
-            // Check if the file is still being written to
-            fs::copy(
-                downloads_path.join(FILE_NAME),
-                destination_path.join(FILE_NAME),
-            )
-            .unwrap();
+        for file in config["sources"]
+            .values()
+            .filter_map(|f| f.clone().filter(|f| f.len() > 0))
+        {
+            let file_str = file.to_owned();
 
-            fs::remove_file(downloads_path.join(FILE_NAME)).unwrap();
+            if downloads_path.join(&file_str).exists() {
+                // Check if the file is still being written to
+                fs::copy(
+                    downloads_path.join(&file_str),
+                    destination_path.join(&file_str),
+                )
+                .unwrap();
 
-            println!("Moved {} to {}.", FILE_NAME, DESTINATION_FOLDER);
+                fs::remove_file(downloads_path.join(&file_str)).unwrap();
+
+                println!("Moved {} to {}.", &file_str, DESTINATION_FOLDER);
+            }
         }
 
         // Sleep for 1 second before checking again
