fe16c07b6c5508cedfb530107293f87b8a7b1a47
Sharun <sharunksplus@gmail.com>
2023-05-09 21:21:17 -0400

    updated build script

--- build.rs
+++ build.rs
@@ -2,4 +2,6 @@ use windres::Build;
 
 fn main() {
     Build::new().compile("icons/app-resources.rc").unwrap();
+    println!("cargo:rerun-if-changed=icons/app-resources.rc");
+    println!("cargo:rerun-if-changed=icons/app-icon.ico");
 }
