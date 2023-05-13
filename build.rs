b1c575425e3a004d0d328cd881f66cffce47b5cd
Sharun <sharunksplus@gmail.com>
2023-05-12 21:51:19 -0400

    update build script to kill running instance of janitor on build

--- build.rs
+++ build.rs
@@ -1,6 +1,15 @@
+use std::process::Command;
+
 use windres::Build;
 
 fn main() {
+    #[cfg(any(target_os = "windows"))]
+    {
+        Command::new("taskkill")
+            .args(&["/F", "/IM", "janitor.exe"])
+            .spawn()
+            .unwrap(); // replace with your command and arguments
+    }
     Build::new().compile("icons/app-resources.rc").unwrap();
     println!("cargo:rerun-if-changed=icons/app-resources.rc");
     println!("cargo:rerun-if-changed=icons/app-icon.ico");
