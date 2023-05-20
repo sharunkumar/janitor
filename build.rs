0c0dcb92b80667c2db40dda6fe48ff934a669b25
Sharun <sharunksplus@gmail.com>
2023-05-20 19:17:34 -0400

    update build scripts

--- build.rs
+++ build.rs
@@ -1,19 +1,15 @@
-#[cfg(windows)]
-extern crate windres;
-
-use std::process::Command;
-
-use windres::Build;
-
 fn main() {
     #[cfg(any(target_os = "windows"))]
     {
+        extern crate windres;
+        use std::process::Command;
+        use windres::Build;
         Command::new("taskkill")
             .args(&["/F", "/IM", "janitor.exe"])
             .spawn()
             .unwrap();
+        Build::new().compile("icons/app-resources.rc").unwrap();
+        println!("cargo:rerun-if-changed=build.rs");
+        println!("cargo:rerun-if-changed=icons/");
     }
-    Build::new().compile("icons/app-resources.rc").unwrap();
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=icons/");
 }
