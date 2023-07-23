9ec0909132397e7bbe4d7f3ee3d0c867d82d9e2b
Sharun <sharunksplus@gmail.com>
2023-07-23 17:41:41 -0400

    added named resource rather than bundling the icon

--- src/icons.rs
+++ src/icons.rs
@@ -5,18 +5,7 @@ pub fn get_app_icon() -> IconSource {
     return IconSource::Resource("aa-exe-icon");
 
     #[cfg(not(target_os = "windows"))]
-    return {
-        let cursor_red = std::io::Cursor::new(include_bytes!("../icons/png/app-icon-32.png"));
-        let decoder_red = png::Decoder::new(cursor_red);
-        let (info_red, mut reader_red) = decoder_red.read_info().unwrap();
-        let mut buf_red = vec![0; info_red.buffer_size()];
-        reader_red.next_frame(&mut buf_red).unwrap();
-        IconSource::Data {
-            data: buf_red,
-            height: 32,
-            width: 32,
-        }
-    };
+    return IconSource::Resource("user-trash");
 }
 
 pub fn get_blue_icon() -> IconSource {
@@ -24,16 +13,5 @@ pub fn get_blue_icon() -> IconSource {
     return IconSource::Resource("fire-blue");
 
     #[cfg(not(target_os = "windows"))]
-    return {
-        let cursor_red = std::io::Cursor::new(include_bytes!("../icons/png/fire-blue-32.png"));
-        let decoder_red = png::Decoder::new(cursor_red);
-        let (info_red, mut reader_red) = decoder_red.read_info().unwrap();
-        let mut buf_red = vec![0; info_red.buffer_size()];
-        reader_red.next_frame(&mut buf_red).unwrap();
-        IconSource::Data {
-            data: buf_red,
-            height: 32,
-            width: 32,
-        }
-    };
+    return IconSource::Resource("user-trash-full");
 }
